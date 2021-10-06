5ff19d7578f8d42827fcd276ffa8fc53a1cc6bdd
Ethan Wu <ethanwu@cs.ucsb.edu>
2021-10-05 23:10:14 -0700

    Add options for pruning old records

--- monitor/__main__.py
+++ monitor/__main__.py
@@ -9,16 +9,17 @@ import pathlib
 import sqlite3
 import time
 
-from . import DEFAULT_TIMESTEP, RECORD_TABLE
+from . import DEFAULT_PRUNING_FREQUENCY, DEFAULT_PRUNING_THRESHOLD, \
+    DEFAULT_TIMESTEP, RECORD_TABLE
 from .record import MonitorRecord
+from .util import throttle
 
-
-def main(args: argparse.Namespace):
-    ctx = sqlite3.connect(args.db)
-
-    with ctx:
-        # Create the table in the db.
-        ctx.execute(f'''CREATE TABLE IF NOT EXISTS {RECORD_TABLE} (
+def create_table(ctx):
+    """
+    Create the table for the monitoring records, if it does not already exist.
+    """
+    ctx.execute(f'''
+        CREATE TABLE IF NOT EXISTS {RECORD_TABLE} (
             -- The timestamp corresponding to this record, in UTC time.
             timestamp TEXT,
             -- The percentage of the CPU used.
@@ -35,6 +36,27 @@ def main(args: argparse.Namespace):
 
             PRIMARY KEY (timestamp)
         );''')
+
+def prune_old_records(ctx, max_age):
+    """Prunes old records from the database."""
+    ctx.execute(f'''
+        DELETE FROM {RECORD_TABLE} WHERE
+            strftime('%s', 'now') - timestamp > ?
+        ;''', (
+            max_age,
+        ),
+    )
+
+
+
+def main(args: argparse.Namespace):
+    ctx = sqlite3.connect(args.db)
+
+    pruning_frequency = 0 if not args.prune else args.pruning_frequency
+    prune = throttle(prune_old_records, pruning_frequency)
+
+    with ctx:
+        create_table(ctx)
         # SQLite for Python is set up to execute batches of statements, so
         # we need to manually tell it to update the database.
         ctx.commit()
@@ -49,6 +71,8 @@ def main(args: argparse.Namespace):
 
             rec = MonitorRecord(time.time(), initial_rec)
             rec.write(ctx)
+
+            prune(ctx, args.pruning_threshold)
             ctx.commit()
 
 
@@ -64,5 +88,22 @@ if __name__ == '__main__':
         type=int,
         default=DEFAULT_TIMESTEP,
         help='How often to poll for new stats, in seconds.',
+    )
+    parser.add_argument(
+        '-p', '--prune',
+        action='store_true',
+        help='Prune old entries from the database.',
+    )
+    parser.add_argument(
+        '--pruning-frequency',
+        type=int,
+        default=DEFAULT_PRUNING_FREQUENCY,
+        help='How often to prune outdated records from the table, in multiples of the timestep.',
+    )
+    parser.add_argument(
+        '--pruning-threshold',
+        type=int,
+        default=DEFAULT_PRUNING_THRESHOLD,
+        help='Minimum age for records to be kept while pruning old records, in seconds.',
     )
     main(parser.parse_args())
---
+++ monitor/util.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+def throttle(f, n):
+    """
+    Throttle a function ``f`` such that it is only executed every ``n`` calls.
+    The returned function will execute on its first call. If passed ``n <= 0``,
+    the returned function will be always execute.
+    """
+    i = n
+    def throttled(*args, **kwargs):
+        nonlocal i
+        i += 1
+        if i >= n:
+            i = 0
+            return f(*args, **kwargs)
+
+    return throttled
+
+class TestUtils(unittest.TestCase):
+    def test_throttle(self):
+        f = lambda x: 2 * x
+        throttled = throttle(f, 4)
+
+        for _ in range(30):
+            self.assertEqual(f(3), throttled(3))
+            self.assertIsNone(throttled(3))
+            self.assertIsNone(throttled(3))
+            self.assertIsNone(throttled(3))
