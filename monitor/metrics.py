2f519bc11c69eb6c535490c25f40f9e83524c56f
Ethan Wu <ethanwu@cs.ucsb.edu>
2021-10-12 01:26:45 -0700

    Switch to using `psutil` for net stats

--- monitor/metrics.py
+++ monitor/metrics.py
@@ -23,13 +23,4 @@ def get_mem_usage():
 
 def get_total_packets_received_this_session():
     """Get the total number of packets received in this session."""
-    total = 0
-    lines = subprocess.run(
-        ['ifconfig', '-a'],
-        stdout=subprocess.PIPE
-    ).stdout.decode('ascii').split('\n')
-    for line in lines:
-        m = re.search(PACKETS_RECEIVED, line)
-        if m:
-            total += int(m.group(1))
-    return total
+    return psutil.net_io_counters().packets_recv
